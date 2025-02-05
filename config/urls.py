2f62862524b609ef558317d21f79af56fe173048
SeungminAhn <smahn9123@gmail.com>
2025-02-05 18:24:33 +0900

    ✨ add basic home page

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
+    path("", views.HomeView.as_view(), name="home"),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("blog/", include("blog.urls")),
