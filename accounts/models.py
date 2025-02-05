827e2ea93ff98020aec68dcb7575fb6633c1038e
SeungminAhn <smahn9123@gmail.com>
2025-02-05 11:03:01 +0900

    ✨ add BlogUser model as AUTH_USER_MODEL

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class BlogUser(AbstractUser):
+    nickname = models.CharField(max_length=30, verbose_name="닉네임")
+    profile_img = models.ImageField(
+        upload_to="profile/", blank=True, verbose_name="프로필사진"
+    )
+
+    class Meta:
+        verbose_name = "사용자"
+        verbose_name_plural = "사용자 목록"
