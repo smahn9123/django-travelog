d73ef086f4d21944cd7df19ff2aa9ef78e6b168e
SeungminAhn <smahn9123@gmail.com>
2025-02-05 17:47:57 +0900

    ✨ implement blog app basic feature

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.PostListView.as_view(), name="post_list"),
+    path("<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("write/", views.PostWriteView.as_view(), name="post_write"),
+    path("edit/<int:pk>/", views.PostUpdateView.as_view(), name="post_edit"),
+    path("delete/<int:pk>/", views.PostDeleteView.as_view(), name="post_delete"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.urls import reverse, reverse_lazy
+from .forms import PostForm
+from .models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    ordering = "-created_at"
+    context_object_name = "posts"
+    # template_name = "blog/post_list.html"
+    # paginate_by = 3
+
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = "post"
+
+
+class PostWriteView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    login_url = reverse_lazy("accounts_login")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("post_detail", kwargs={"pk": self.object.pk})
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    # template_name = "blog/post_form.html"
+    login_url = reverse_lazy("accounts_login")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("post_detail", kwargs={"pk": self.object.pk})
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy("post_list")
+    login_url = reverse_lazy("accounts_login")
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
