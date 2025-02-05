6418c4d08b0cfbd1e560470a5558137349553ffc
SeungminAhn <smahn9123@gmail.com>
2025-02-05 15:01:53 +0900

    🐛 fix wrong BlogUser model fields

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 
 class BlogUser(AbstractUser):
-    nickname = models.CharField(max_length=30, verbose_name="닉네임")
+    nickname = models.CharField(max_length=30, unique=True, verbose_name="닉네임")
     profile_img = models.ImageField(
-        upload_to="profile/", blank=True, verbose_name="프로필사진"
+        upload_to="profile/", blank=True, null=True, verbose_name="프로필사진"
+    )
+    bio = models.TextField(
+        max_length=500, blank=True, default="", verbose_name="자기소개"
     )
 
     class Meta:
